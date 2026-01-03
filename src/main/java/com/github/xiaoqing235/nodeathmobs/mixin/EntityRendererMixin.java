7c19ebeca5902a49edf4abfb7cd1a6b37c686ac7
XiaoQing235 <a1753059601@163.com>
2026-01-03 23:24:46 +0800

    first commit

---
+++ src/main/java/com/github/xiaoqing235/nodeathmobs/mixin/EntityRendererMixin.java
@@ -0,0 +1,23 @@
+package com.github.xiaoqing235.nodeathmobs.mixin;
+
+import net.minecraft.client.render.Frustum;
+import net.minecraft.client.render.entity.EntityRenderer;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EntityRenderer.class)
+public abstract class EntityRendererMixin
+{
+    @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
+    private void shouldNotRenderDead(Entity entity, Frustum frustum, double x, double y, double z, CallbackInfoReturnable<Boolean> cir){
+        if (entity instanceof LivingEntity && ((LivingEntity) entity).getHealth() <= 0f)
+        {
+            cir.setReturnValue(false);
+        }
+    }
+}
---
+++ src/main/java/com/github/xiaoqing235/nodeathmobs/mixin/GameRendererMixin.java
@@ -0,0 +1,21 @@
+package com.github.xiaoqing235.nodeathmobs.mixin;
+
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import java.util.function.Predicate;
+
+@Mixin(GameRenderer.class)
+public abstract class GameRendererMixin
+{
+    @ModifyArg(method = "findCrosshairTarget(Lnet/minecraft/entity/Entity;DDF)Lnet/minecraft/util/hit/HitResult;",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/entity/projectile/ProjectileUtil;raycast(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Box;Ljava/util/function/Predicate;D)Lnet/minecraft/util/hit/EntityHitResult;"))
+    private Predicate<Entity> doNotFindDeadTarget(Predicate<Entity> predicate)
+    {
+        return predicate.and((entityIn) -> !(entityIn instanceof LivingEntity) || ((LivingEntity) entityIn).getHealth() > 0f);
+    }
+}
